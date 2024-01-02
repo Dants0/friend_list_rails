1ec108c18751ad2a0dfc8f1baca49f6b9b015881
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2024-01-02 16:10:25 -0300

    (add): implementando banco de dados (scaffold)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :friends
+
   root 'home#index'
 
   get '/home/about'
+
+  get '/friends'
+  get '/friends/new'
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
